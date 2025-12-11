0c0237a40f635350e293fd7e2c1e2b937f779c49
Gabriella Korinfsky <101523340+gabriellakorinfsky@users.noreply.github.com>
2025-12-10 22:39:47 -0300

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware para permitir requisições de diferentes origens (CORS) e interpretar JSON
 app.use(cors({
-  origin: 'https://blackout-finance-w4lj6kstk-kauas-projects-5d7f1c1a.vercel.app', // link do front
+  origin: 'https://blackout-finance-ui.vercel.app/', // link do front
   credentials: true }));
 
 app.use(express.json());
